4a2cd778622e6553770e037a7eea304dd9bfea12
Cainã Machado <cwaxtd@gmail.com>
2020-12-16 18:05:07 -0300

    Starting calculator project

---
+++ RestWithASP.NET5/RestWithASP.NET5/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP.NET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            bool isNumber = double.TryParse(strNumber, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out double number);
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            if (decimal.TryParse(strNumber, out decimal decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+    }
+}
