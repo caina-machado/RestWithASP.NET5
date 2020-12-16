1207b0b330f17baaab45fb0d53110df36c01c4e8
Cainã Machado <cwaxtd@gmail.com>
2020-12-16 18:41:08 -0300

    Starting person model

---
+++ RestWithASP.NET5/RestWithASP.NET5/Controllers/PersonController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASP.NET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Sum()
+        {
+            return BadRequest("Invalid input");
+        }
+    }
+}
