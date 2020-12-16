aa16f38a2311e0a7d520dd7d7c6c3c5b7ec97b4f
Cainã Machado <cwaxtd@gmail.com>
2020-12-16 20:48:06 -0300

    Database connection and al crud inplemented

--- RestWithASP.NET5/RestWithASP.NET5/Controllers/PersonController.cs
+++ RestWithASP.NET5/RestWithASP.NET5/Controllers/PersonController.cs
@@ -1,15 +1,66 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASP.NET5.Model;
+using RestWithASP.NET5.Services;
 
 namespace RestWithASP.NET5.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class PersonController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Sum()
+        private readonly IPersonService _personService;
+
+        public PersonController(IPersonService personService)
         {
-            return BadRequest("Invalid input");
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult FindAll()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult FindById(long id)
+        {
+            var person = _personService.FindById(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
         }
     }
 }
---
+++ RestWithASP.NET5/RestWithASP.NET5/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASP.NET5.Model;
+using System.Collections.Generic;
+
+namespace RestWithASP.NET5.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        List<Person> FindAll();
+        Person FindById(long id);
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
